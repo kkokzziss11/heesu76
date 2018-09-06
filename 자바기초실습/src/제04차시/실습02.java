1690cf40610e60348ed0d5a1c79727b86eedaca1
user <user@JBTC10105>
2018-09-06 14:56:25 +0900

    sl

---
+++ 자바기초실습/src/제04차시/실습02.java
@@ -0,0 +1,18 @@
+
+package 제04차시;
+import java.io.*;
+public class 실습02 {
+
+	public static void main(String[] args) {
+		int bt;
+		try {
+			while((bt = System.in.read())!= -1) {
+				System.out.print((char)bt);
+			}
+		}catch (IOException e) {
+			System.err.println(e);
+		}
+
+	}
+
+}

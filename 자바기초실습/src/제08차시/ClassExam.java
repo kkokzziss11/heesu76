f70a2e1094bc436d57516a1a1e61c4ac0e91cf8b
user <user@JBTC10105>
2018-10-18 15:22:26 +0900

    z

--- 자바기초실습/src/제08차시/ClassExam.java
+++ 자바기초실습/src/제08차시/ClassExam.java
@@ -5,7 +5,7 @@ public class ClassExam {
 		Student kim = new Student();
 		kim.name = "김현우";
 		kim.grade=2;
-		System.out.println("학생의 이름은"+kim.name+"이고"+kim.grade+"학년입니다");
+		System.out.println("학생의 이름은"+kim.name+"이고,"+kim.grade+"학년입니다");
 				
 	}
 
